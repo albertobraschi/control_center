e845f066a2295eec64e0c7ce6b7992d9ca2b21b4
Alan Johnson <alan@gnoso.com>
2008-11-02 15:04:16 -0500

    Fixed problems in installation script.

--- install.rb
+++ install.rb
@@ -10,15 +10,15 @@ puts "Installing Control Center..."
 # copy our files into the project
 puts "Copying stylesheet..."
 File.copy(resources_dir + "/control_center.css", 
-    RAILS_DIR + "/public/stylesheets/control_center.css")
+    RAILS_ROOT + "/public/stylesheets/control_center.css")
 
 puts "Copying layout..."
 File.copy(resources_dir + "/control_center.html.erb",
-    RAILS_DIR + "/app/views/layouts/control_center.html.erb")
+    RAILS_ROOT + "/app/views/layouts/control_center.html.erb")
 
 puts "Copying initializer..."
 File.copy(resources_dir + "/control_center.html.erb",
-    RAILS_DIR + "/config/initializers/control_center.rb")
+    RAILS_ROOT + "/config/initializers/control_center.rb")
 
 # print out the readme
 puts # readme content gets printed here
