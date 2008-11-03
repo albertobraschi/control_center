74f0da83863895868ccbbbf23fe8374c3787f837
Alan Johnson <alan@gnoso.com>
2008-11-03 16:09:03 -0500

    Updated control center library to work correctly.

--- lib/control_center.rb
+++ lib/control_center.rb
@@ -3,4 +3,14 @@ class ControlCenter
   cattr_accessor :header_links
   cattr_accessor :tabs
   cattr_accessor :theme_color
+  
+  def self.add_header_link(text, url)
+    ControlCenter.header_links ||= []
+    ControlCenter.header_links << [text, url]
+  end
+  
+  def self.add_tab(text, url)
+    ControlCenter.tabs ||= []
+    ControlCenter.tabs << [text, url]
+  end
 end
