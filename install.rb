b9e207a4bf6c594eb9da280e261540c6d11b791b
Alan Johnson <alan@gnoso.com>
2008-11-03 20:39:58 -0500

    Tweaked control center to simply use partials.

--- install.rb
+++ install.rb
@@ -1,6 +1,20 @@
 # installs our control center stuff
 require 'ftools'
 
+# quick method for copying files only if they don't already
+# exist
+def install(source, target)
+  print "Copying #{source}..."
+  
+  if File.exists?(resources_dir + source)
+    File.copy(resources_dir + source,
+        target)
+    puts "Done!"
+  else
+    puts "File exists, Skipping."
+  end
+end
+
 # get some basic vars set up
 resources_dir = File.dirname(__FILE__) + "/resources"
 
@@ -9,21 +23,18 @@ puts
 puts "Installing Control Center..."
 
 # copy our files into the project
-print "Copying stylesheet... "
-File.copy(resources_dir + "/control_center.css", 
+install("/control_center.css", 
     RAILS_ROOT + "/public/stylesheets/control_center.css")
-puts "Done!"
 
-print "Copying layout... "
-File.copy(resources_dir + "/control_center.html.erb",
+install("/control_center.rb",
+    RAILS_ROOT + "/config/initializers/control_center.rb")
+    
+install("/control_center.html.erb",
     RAILS_ROOT + "/app/views/layouts/control_center.html.erb")
-puts "Done!"
-
-print "Copying initializer... "
-File.copy(resources_dir + "/control_center.rb",
-    RAILS_ROOT + "/config/initializers/control_center.rb")    
-puts "Done!"
-
+install("/_header_links.html.erb",
+    RAILS_ROOT + "/app/views/layouts/_header_links.html.erb")
+install("/_tabs.html.erb",
+    RAILS_ROOT + "/app/views/layouts/_tabs.html.erb")
 # puts some space in here
 puts
 puts
--- lib/control_center.rb
+++ lib/control_center.rb
@@ -1,16 +1,6 @@
 class ControlCenter
+  
   cattr_accessor :title
-  cattr_accessor :header_links
-  cattr_accessor :tabs
   cattr_accessor :theme_color
   
-  def self.add_header_link(text, &block)
-    ControlCenter.header_links ||= []
-    ControlCenter.header_links << [text, block]
-  end
-  
-  def self.add_tab(text, &block)
-    ControlCenter.tabs ||= []
-    ControlCenter.tabs << [text, block]
-  end
 end
