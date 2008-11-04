6f274d8860290f8452dee45184e7280896bb580a
Alan Johnson <alan@gnoso.com>
2008-11-03 20:47:09 -0500

    Updated initializer to work with new partials.

--- resources/control_center.rb
+++ resources/control_center.rb
@@ -2,12 +2,4 @@
 # preferences
 
 ControlCenter.title = "Control Center"
-
-ControlCenter.add_header_link("Some") { "/some" }
-ControlCenter.add_header_link("Link") { "/link" }
-
-ControlCenter.add_tab("Tab1") { "/tab1" }
-ControlCenter.add_tab("Test") { "/test" }
-ControlCenter.add_tab("Tab3") { "/tab3" }
-
 ControlCenter.theme_color = "#0033CC"
