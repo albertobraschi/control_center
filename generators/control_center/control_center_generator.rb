e9a9311a71fe3eb05dbd11b7be8616a3db2fd845
codebrulee <kevin@codebrulee.com>
2009-02-27 17:13:22 -0500

    Adding subtab support.

--- generators/control_center/control_center_generator.rb
+++ generators/control_center/control_center_generator.rb
@@ -15,6 +15,8 @@ class ControlCenterGenerator < Rails::Generator::Base
       m.directory(layouts_path)
       m.file('_tabs.html.erb', 
           File.join(layouts_path, "_tabs.html.erb"))
+      m.file('_tabs.html.erb', 
+          File.join(layouts_path, "_sub_nav.html.erb"))
       m.file('_header_links.html.erb',
           File.join(layouts_path, "_header_links.html.erb"))
       m.file('control_center.html.erb',
--- lib/helpers.rb
+++ lib/helpers.rb
@@ -11,8 +11,22 @@ module ControlCenter::Helpers
       link_to tab_name, url
     end
   end
+  def sub_tab(sub_tab_name, url)
+    options = {}
+    if @control_center_sub_tab == sub_tab_name
+      options[:class] = "active" 
+    else
+      options[:style] = "background-color: #{ControlCenter.theme_color};"
+    end
+    content_tag "li", options, true do
+      link_to sub_tab_name, url
+    end
+  end
   
   def select_tab(tab)
     @control_center_tab = tab
+  end
+  def select_sub_tab(sub_tab)
+    @control_center_sub_tab = sub_tab
   end
 end
