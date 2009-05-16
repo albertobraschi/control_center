fec2dd42b19ed6837de8f4f134b15b2a8f7e4d27
Alan Johnson <alan@gnoso.com>
2009-05-16 15:07:45 -0400

    Added config example to the initializer. Got javscript includes working.

--- generators/control_center/templates/control_center.rb
+++ generators/control_center/templates/control_center.rb
@@ -3,3 +3,12 @@
 
 ControlCenter::Config.app_title = "Control Center"
 ControlCenter::Config.theme_color = "#0033CC"
+
+# Custom Stylesheets
+# ControlCenter::Config.custom_css = { 
+#   "custom_css_1" => { :media => :all }, 
+#   "custom_css_2" => { :media => :print }
+# }
+
+# Custom JavaScript Includes
+#ControlCenter::Config.include_javascript = %w(:defaults)
--- lib/control_center.rb
+++ lib/control_center.rb
@@ -3,9 +3,14 @@ module ControlCenter
     cattr_accessor :app_title
     cattr_accessor :theme_color
     cattr_accessor :custom_css
+    cattr_accessor :include_javascript
     
     def self.custom_css
       @@custom_css ||= {}
+    end
+    
+    def self.include_javascript
+      @@include_javascript || []
     end
   end
 end
